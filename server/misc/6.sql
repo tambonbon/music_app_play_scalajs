309c2062053f2312dafe6f1ebcfe141680940391
tambonbon <nttammy@yahoo.com>
2021-03-20 23:50:53 +0100

    Initial Commit

--- server/misc/6.sql
+++
@@ -1,8 +0,0 @@
-# --- !Ups
-
-ALTER TABLE "songs" RENAME COLUMN "id" to "songId";
-
-# --- !Downs
-
-drop table "songs" if exists;
-
--- server/misc/9.sql
+++
@@ -1,7 +0,0 @@
-# --- !Ups
-
-ALTER TABLE "playing" ADD "user" varchar;
-
-# --- !Downs
-
-drop table "playing" if exists;
