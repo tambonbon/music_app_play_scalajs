14ab6f776a8414b61bf425f6bb342c0400ff2769
tambonbon <nttammy@yahoo.com>
2021-05-30 21:22:21 +0200

    ScalaJS up and running; fix build.sbt for client; change endpoints in Main.scala

--- client/src/main/scala/Main.scala
+++ client/src/main/scala/Main.scala
@@ -3,11 +3,8 @@ import shared.SharedMessage
 object Main {
   def main(args: Array[String]): Unit = {
     println("This is Scala.js")
-//    println(ScalaJSVersion.albumsRoute)
-    println(ScalaJSVersion.addAlbumRoute)
-    println(ScalaJSVersion.testArtist)
-//    if (dom.document.getElementById("scalaJsShoutout")!= null){
-//      dom.document.getElementById("scalaJsShoutout").textContent = SharedMessage.itWorks
-//    }
+    if (dom.document.getElementById("scalaJsShoutout")!= null){
+      dom.document.getElementById("scalaJsShoutout").textContent = SharedMessage.itWorks
+    }
   }
 }
