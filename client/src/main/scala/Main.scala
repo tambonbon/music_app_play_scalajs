7ce80fcf21969a8d04761b63c3b95067e3616877
tambonbon <nttammy@yahoo.com>
2021-05-31 10:56:46 +0200

    tweak Main.scala for demonstration

--- client/src/main/scala/Main.scala
+++ client/src/main/scala/Main.scala
@@ -1,10 +1,28 @@
 import org.scalajs.dom
+import org.scalajs.dom.{document, html}
 import shared.SharedMessage
+
+import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 object Main {
   def main(args: Array[String]): Unit = {
     println("This is Scala.js")
-    if (dom.document.getElementById("scalaJsShoutout")!= null){
-      dom.document.getElementById("scalaJsShoutout").textContent = SharedMessage.itWorks
+    if (document.getElementById("scalaJsShoutout")!= null){
+      document.getElementById("scalaJsShoutout").textContent = SharedMessage.itWorks
     }
+
+    appendParagraph(document.getElementById("scalaJsShoutout"), "This is a new paragraph")
+  }
+
+  def appendParagraph(target: dom.Node, text: String): Unit = {
+    val p = document.createElement("p")
+    val textNode = document.createTextNode(text)
+    p.appendChild(textNode)
+    target.appendChild(p)
+  }
+
+//  @JSExportTopLevel("clickStuff")
+  def click(): Unit = {
+    println("Button clicked")
+    appendParagraph(document.getElementById("scalaJsShoutout"), "Button clicked")
   }
 }
